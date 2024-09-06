e45eeee67e25d575d6cc8a08437bd6aeabc9bef5
cameron-p-carter <cameron.cz@outlook.com>
2024-09-06 19:27:33 +1000

    changes to schema and rebuilt the database in docker

--- backend/index.js
+++ backend/index.js
@@ -24,72 +24,72 @@ app.get('/test', (req, res) => {
   }
 });
 
-//get all users
-app.get('/users', async (req, res) => {
+//get all notes
+app.get('/notes', async (req, res) => {
   try {
-    const users = await prisma.user.findMany();
-    res.status(200).json(users);
+    const notes = await prisma.note.findMany();
+    res.status(200).json(notes);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-//get user by id
-app.get('/users/:id', async (req, res) => {
+//get note by id
+app.get('/notes/:id', async (req, res) => {
   try {
-    const user = await prisma.user.findUnique({
+    const note = await prisma.note.findUnique({
       where: {
         id: Number(req.params.id),
       },
     });
-    res.status(200).json(user);
+    res.status(200).json(note);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-//create user
-app.post('/users', async (req, res) => {
+//create note
+app.post('/notes', async (req, res) => {
   try {
-    const user = await prisma.user.create({
+    const note = await prisma.note.create({
       data: {
-        name: req.body.name,
-        email: req.body.email
+        title: req.body.title,
+        content: req.body.content
       },
     });
-    res.status(201).json(user);
+    res.status(201).json(note);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-//update user
-app.put('/users/:id', async (req, res) => {
+//update note
+app.put('/notes/:id', async (req, res) => {
   try {
-    const user = await prisma.user.update({
+    const note = await prisma.note.update({
       where: {
         id: Number(req.params.id),
       },
       data: {
-        name: req.body.name,
-        email: req.body.email
+        title: req.body.title,
+        content: req.body.content
       },
     });
-    res.status(200).json(user);
+    res.status(200).json(note);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-//delete user
-app.delete('/users/:id', async (req, res) => {
+//delete note
+app.delete('/notes/:id', async (req, res) => {
   try {
-    const user = await prisma.user.delete({
+    const note = await prisma.note.delete({
       where: {
         id: Number(req.params.id),
       },
     });
-    res.status(200).json(user);
+    res.status(200).json(note);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
